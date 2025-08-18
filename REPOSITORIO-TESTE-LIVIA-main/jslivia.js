1265720c0791ce167c838655328f6871860cd913
daironromano <liviaa.mesquitax@gmail.com>
2025-08-18 10:18:49 -0300

    AULA 18/08
    arquivo js

--- REPOSITORIO-TESTE-LIVIA-main/jslivia.js
+++ REPOSITORIO-TESTE-LIVIA-main/jslivia.js
@@ -1,17 +1,11 @@
-let numero01 = parseInt(prompt("Digite um número: "));
-let numero02 = parseInt(prompt("Digite outro número: "));
+let A = parseInt(prompt("Digite um número A: "));
+let B = parseInt(prompt("Digite outro número B: "));
+let C;
 
-if (numero01 == numero02) {
-  alert("Os número são iguais");
-} else if (numero01 != numero02) {
-  alert("Os número são diferentes");
-}
-if (numero01 < numero02) {
-  alert(
-    `Você digitou os números ${numero01} e ${numero02}, em ordem crescente.`
-  );
-} else if (numero01 > numero02) {
-  alert(
-    `Você digitou os números ${numero02} e ${numero01}, em ordem crescente.`
-  );
+if (A == B) {
+  C = A + B;
+  alert(`A SOMA DE A E B É ${C}`);
+} else if (A != B) {
+  C = A * B;
+  alert(`A MULTIPLICAÇÃO DE A E B É ${C}`);
 }
